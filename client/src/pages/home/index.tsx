960e812cc5f78799ca8dad601a1229c2b19e5951
Giulyano Santos <giulyanofelipesantos@gmail.com>
2023-06-09 12:00:27 -0300

    Começando seção carrossel

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import WhoMadeAthena from "../../components/WhoMadeAthena";
+import WhoMadeAthena from "../../components/WhoMadeAthena"; 
 import ContactUs from "../../components/ContactUs";
+import Carrossel from "../../components/Carrossel";
 
 
 
 
 export const Home: React.FC = () => {
     return (<>
-     
+     <Carrossel/>
      <WhoMadeAthena />
      <ContactUs/>
  
